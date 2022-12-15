db871230f8bdbe4df8f18cad4512c7a03b658dbe
MyJules <mudryk200@gmail.com>
2022-12-15 02:20:45 +0200

    Working detection

--- Detector/src/filters/goodfeaturestotrack.cpp
+++ Detector/src/filters/goodfeaturestotrack.cpp
@@ -14,8 +14,7 @@ GoodFeaturesToTrack::~GoodFeaturesToTrack()
 
 cv::Mat GoodFeaturesToTrack::process(const cv::Mat& image)
 {
-    cv::Mat result = image;
-    cv::cvtColor(result, result, cv::COLOR_BGR2GRAY);
+    using namespace cv;
 
     if(m_isNewImageToTrack)
     {
@@ -23,13 +22,68 @@ cv::Mat GoodFeaturesToTrack::process(const cv::Mat& image)
         m_isNewImageToTrack = false;
     }
 
-    std::vector<cv::Point2f> corners;
-    cv::goodFeaturesToTrack(result, corners, 25, 0.01, 10);
+    if(!m_imageToTrack) return image;
 
-    for(int i = 0; i < corners.size(); i++)
-    {
-        cv::circle(result, corners[i], 4, 255, 1);
-    }
+    cv::Mat result = image;
+    cv::cvtColor(result, result, cv::COLOR_BGR2GRAY);
+
+    //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+    int minHessian = 400;
+    Ptr<SIFT> detector = SIFT::create( minHessian );
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
+    Mat descriptors_object, descriptors_scene;
+    detector->detectAndCompute( *m_imageToTrack, noArray(), keypoints_object, descriptors_object );
+    detector->detectAndCompute( image, noArray(), keypoints_scene, descriptors_scene );
+
+      //-- Step 2: Matching descriptor vectors with a FLANN based matcher
+     // Since SURF is a floating-point descriptor NORM_L2 is used
+     Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+     std::vector< std::vector<DMatch> > knn_matches;
+     matcher->knnMatch( descriptors_object, descriptors_scene, knn_matches, 2 );
+
+     //-- Filter matches using the Lowe's ratio test
+     const float ratio_thresh = 0.75f;
+     std::vector<DMatch> good_matches;
+     for (size_t i = 0; i < knn_matches.size(); i++)
+     {
+         if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
+         {
+             good_matches.push_back(knn_matches[i][0]);
+         }
+     }
+     //-- Draw matches
+     Mat img_matches;
+     //-- Localize the object
+     std::vector<Point2f> obj;
+     std::vector<Point2f> scene;
+     for( size_t i = 0; i < good_matches.size(); i++ )
+     {
+         //-- Get the keypoints from the good matches
+         obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+         scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+     }
+
+     if(obj.size() < 4 && scene.size() < 4) return result;
+
+     Mat H = findHomography( obj, scene, RANSAC );
+     //-- Get the corners from the image_1 ( the object to be "detected" )
+     std::vector<Point2f> obj_corners(4);
+     obj_corners[0] = Point2f(0, 0);
+     obj_corners[1] = Point2f( (float)m_imageToTrack->cols, 0 );
+     obj_corners[2] = Point2f( (float)m_imageToTrack->cols, (float)m_imageToTrack->rows );
+     obj_corners[3] = Point2f( 0, (float)m_imageToTrack->rows );
+     std::vector<Point2f> scene_corners(4);
+     perspectiveTransform( obj_corners, scene_corners, H);
+
+     //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+     line( result, scene_corners[0] + Point2f(0, 0),
+                   scene_corners[1] + Point2f(0, 0), Scalar(0, 255, 0), 4 );
+     line( result, scene_corners[1] + Point2f(0, 0),
+                   scene_corners[2] + Point2f(0, 0), Scalar( 0, 255, 0), 4 );
+     line( result, scene_corners[2] + Point2f(0, 0),
+                   scene_corners[3] + Point2f(0, 0), Scalar( 0, 255, 0), 4 );
+     line( result, scene_corners[3] + Point2f(0, 0),
+                   scene_corners[0] + Point2f(0, 0), Scalar( 0, 255, 0), 4 );
 
     return result;
 }
